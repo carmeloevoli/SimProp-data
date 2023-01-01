5d2e99d98b8a2a742e97ecc53dfa7c644d178274
carmeloevoli <carmelo.evoli@gssi.it>
2023-01-01 19:16:49 +0100

    add BBG2006 losses

---
+++ losses/convert.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def load_losses(filename):
+    file = open(filename, 'r')
+    lines = file.readlines()
+    
+    xstr = lines[0].split(',')
+    x = [float(i) for i in xstr]
+
+    ystr = lines[1].split(',')
+    y = [float(i) for i in ystr]
+
+    return np.array(x), np.array(y)
+    
+def dump_losses():
+    filename = 'tables/pairlossesInSimprop.txt'
+    x, y = load_losses(filename)
+    
+    expected_length = 1001
+    file = open('losses_pair_BGG2002.txt', 'w')
+    file.write('# size in energy %d\n' % expected_length)
+    file.write('# log10(E/eV) - log10(b/year^-1) \n')
+    
+    counter = 0
+    for i in range(0, len(x)):
+        counter += 1
+        file.write("%10.5e, %10.5e\n" % (x[i], y[i]))
+    
+    assert(counter == expected_length)
+    file.close()
+
+if __name__== "__main__":
+    dump_losses()
